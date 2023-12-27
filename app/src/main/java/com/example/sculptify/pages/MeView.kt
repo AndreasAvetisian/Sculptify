d675314bf58d93c532df2fd7d92daa95fc697ce5
Andreas Avetisian <t0avan00@students.oamk.fi>
2023-12-27 15:16:26 +0100

    Minor change | Added arrows
    Added arrows to buttons in "Me" page

--- app/src/main/java/com/example/sculptify/pages/MeView.kt
+++ app/src/main/java/com/example/sculptify/pages/MeView.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -198,36 +199,49 @@ fun MBSButton(
             .height(71.25.dp)
             .clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Card (
-            colors = cardColor,
-            modifier = Modifier
-                .width(39.9.dp)
-                .height(39.9.dp)
+        Row (
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Column (
+            Card (
+                colors = cardColor,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .width(39.9.dp)
+                    .height(39.9.dp)
             ) {
-                Icon(
-                    painter = iconId,
-                    tint = Color.White,
-                    contentDescription = iconDescription
-                )
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painter = iconId,
+                        tint = Color.White,
+                        contentDescription = iconDescription
+                    )
+                }
             }
+            Text(
+                modifier = Modifier
+                    .padding(15.675.dp, 0.dp, 0.dp, 0.dp),
+                text = text,
+                fontSize = 17.1.sp,
+                fontFamily = balooFontFamily,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xffFCFCFC)
+            )
         }
-        Text(
-            modifier = Modifier
-                .padding(15.675.dp, 0.dp, 0.dp, 0.dp),
-            text = text,
-            fontSize = 17.1.sp,
-            fontFamily = balooFontFamily,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xffFCFCFC)
-        )
+        Row {
+            Icon(
+                modifier = Modifier
+                    .scale(scaleX = -1f, scaleY = 1f),
+                painter = painterResource(id = R.drawable.arrow),
+                contentDescription = "arrow",
+                tint = Color.White
+            )
+        }
     }
 }
