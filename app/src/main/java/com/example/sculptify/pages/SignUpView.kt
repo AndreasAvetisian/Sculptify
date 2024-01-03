2623661b878700f9f977fe91512f24b96da28ce1
Andreas Avetisian <t0avan00@students.oamk.fi>
2024-01-03 22:49:28 +0100

    SignUp UI implementation

--- app/src/main/java/com/example/sculptify/pages/SignUpView.kt
+++ app/src/main/java/com/example/sculptify/pages/SignUpView.kt
@@ -10,25 +10,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.sculptify.R
 import com.example.sculptify.ui.theme.balooFontFamily
 
 var pageCounter by mutableIntStateOf(1)
+var gender by mutableStateOf("")
 @Composable
 fun SignUpView() {
     Column (
@@ -55,7 +63,8 @@ fun SignUpView() {
 
 @Composable
 fun ConfirmButton(
-    text: String
+    text: String,
+    onClick: () -> Unit
 ) {
     Card (
         colors = CardDefaults.cardColors(Color(0xff0060FE)),
@@ -64,7 +73,7 @@ fun ConfirmButton(
             .fillMaxWidth()
             .height(60.dp)
             .clickable {
-                pageCounter += 1
+                onClick()
             },
     ) {
         Column (
@@ -118,13 +127,18 @@ fun RegistrationBeginning() {
             )
         }
         ConfirmButton(
-            text = "I'M READY"
+            text = "I'M READY",
+            onClick = {
+                pageCounter += 1
+            }
         )
     }
 }
 
 @Composable
 fun Gender() {
+    var selectedButton by remember { mutableStateOf(GenderButton.Male) }
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -135,8 +149,7 @@ fun Gender() {
         Column (
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.90f)
-                .background(Color.Red),
+                .fillMaxHeight(0.90f),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -153,66 +166,111 @@ fun Gender() {
             )
             Row (
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Cyan),
+                    .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Card (
-                    colors = CardDefaults.cardColors(Color(0xff1C1C1E)),
-                    shape = MaterialTheme.shapes.extraLarge,
+                GenderButton(
+                    text = "Male",
+                    textSize = 30.sp,
                     modifier = Modifier
-                        .width(140.dp)
+                        .width(120.dp)
                         .height(120.dp)
-                ) {
-                    Column (
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            text = "Male",
-                            color = Color.White,
-                            fontSize = 30.sp,
-                            fontFamily = balooFontFamily,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                }
-                Card (
-                    colors = CardDefaults.cardColors(Color(0xff1C1C1E)),
-                    shape = MaterialTheme.shapes.extraLarge,
+                        .clickable {
+                            gender = "Male"
+                        },
+                    iconId = R.drawable.male,
+                    tint = Color(0xff0000ff),
+                    iconModifier = Modifier.size(50.dp),
+                    selected = selectedButton == GenderButton.Male
+                )
+                GenderButton(
+                    text = "Female",
+                    textSize = 30.sp,
                     modifier = Modifier
-                        .width(140.dp)
+                        .width(120.dp)
                         .height(120.dp)
-                ) {
-                    Column (
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            text = "Female",
-                            color = Color.White,
-                            fontSize = 30.sp,
-                            fontFamily = balooFontFamily,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                }
+                        .clickable {
+                            gender = "Female"
+                        },
+                    iconId = R.drawable.female,
+                    tint = Color(0xffFFC0CB),
+                    iconModifier = Modifier.size(50.dp),
+                    selected = selectedButton == GenderButton.Female
+                )
             }
+            GenderButton(
+                text = "Others / I'd rather not say",
+                textSize = 20.sp,
+                modifier = Modifier
+                    .padding(start = 45.dp, top= 20.dp, end = 45.dp)
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .clickable {
+                        gender = "Others"
+                    },
+                iconId = R.drawable.female,
+                tint = Color(0xffFFC0CB),
+                iconModifier = Modifier.size(0.dp),
+                selected = selectedButton == GenderButton.Others
+            )
         }
         ConfirmButton(
-            text = "NEXT"
+            text = "NEXT",
+            onClick = {
+                pageCounter += 1
+            }
         )
+    }
+}
+
+enum class GenderButton {
+    Male,
+    Female,
+    Others
+}
+
+@Composable
+fun GenderButton(
+    text: String,
+    textSize: TextUnit,
+    modifier: Modifier,
+    iconId: Int,
+    iconModifier: Modifier,
+    tint: Color,
+    selected: Boolean
+    ) {
+
+    val buttonColor = if (selected) Color(0xff0000ff) else Color(0xff1C1C1E)
+
+    Card (
+        colors = CardDefaults.cardColors(buttonColor),
+        shape = MaterialTheme.shapes.extraLarge,
+        modifier = modifier
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                modifier = iconModifier,
+                painter = painterResource(id = iconId),
+                contentDescription = "",
+                tint = tint
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = text,
+                color = Color.White,
+                fontSize = textSize,
+                fontFamily = balooFontFamily,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
